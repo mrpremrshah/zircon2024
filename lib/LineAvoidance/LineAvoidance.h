4743a3fe3cb9a0aaeb7e7ed6f456c0495359b4be
Dev-R-Github <148823079+Dev-R-Github@users.noreply.github.com>
2024-05-11 13:06:19 -0400

    Update LineAvoidance.h

--- lib/LineAvoidance/LineAvoidance.h
+++ lib/LineAvoidance/LineAvoidance.h
@@ -12,7 +12,7 @@ public:
 
 private:
     const int LINE_SENSOR_THRESHOLD = 50; // Adjust this value based on your sensor readings
-    const int LINE_SENSOR_1 = A0; // change pin numbers for our line sensors(Idk what they are)
+    const int LINE_SENSOR_1 = A0; // change pin numbers for our line sensors(Idk what they are so ask Naveen)
     const int LINE_SENSOR_2 = A1;
     const int LINE_SENSOR_3 = A2;
 };
